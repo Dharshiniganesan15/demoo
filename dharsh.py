bdea448ff4df2cf5f691f1ec880363b4789fcbe2
Dharsh@15 <dharshiniganesan1515@gmail.com>
2026-01-26 20:29:04 +0530

    Add debug parameter to add function for enhanced logging

--- dharsh.py
+++ dharsh.py
@@ -1,12 +1,13 @@
 import datetime
 
-def add(a, b, *args):
+def add(a, b, *args, debug=False):
     """Add multiple numbers together with enhanced logging and validation.
     
     Args:
         a (int/float): First number
         b (int/float): Second number
         *args: Additional numbers to add
+        debug (bool): Enable debug output
         
     Returns:
         int/float: Sum of all numbers
@@ -18,8 +19,15 @@ def add(a, b, *args):
     if not all(isinstance(x, (int, float)) for x in [a, b] + list(args)):
         raise TypeError("All arguments must be numbers")
     
+    if debug:
+        print(f"[DEBUG] Adding: {a} + {b} + {args}")
+    
     print(f"[{datetime.datetime.now()}] Adding numbers")
     result = a + b
     for num in args:
         result += num
+    
+    if debug:
+        print(f"[DEBUG] Result: {result}")
+    
     return result
