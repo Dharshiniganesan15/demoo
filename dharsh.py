104635e45bba10689cee5d64351a390422f60808
Dharsh@15 <dharshiniganesan1515@gmail.com>
2026-01-26 19:43:03 +0530

    Add API documentation and math functions

--- dharsh.py
+++ dharsh.py
@@ -1,1 +1,35 @@
-print("hello")
+def add(a, b):
+    """Add two numbers together.
+    
+    Args:
+        a (int/float): First number
+        b (int/float): Second number
+        
+    Returns:
+        int/float: Sum of a and b
+    """
+    return a + b
+
+def subtract(a, b):
+    """Subtract two numbers.
+    
+    Args:
+        a (int/float): First number
+        b (int/float): Second number
+        
+    Returns:
+        int/float: Difference of a and b
+    """
+    return a - b
+
+def multiply(a, b):
+    """Multiply two numbers.
+    
+    Args:
+        a (int/float): First number
+        b (int/float): Second number
+        
+    Returns:
+        int/float: Product of a and b
+    """
+    return a * b
